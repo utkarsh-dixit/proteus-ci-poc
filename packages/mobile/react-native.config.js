272dab2e2403b6e971ff9d7d35d55b2f3d4a7955
Utkarsh Dixit <utkarsh.dixit@headout.com>
2020-01-15 16:01:07 +0530

    Fix react-native.config.js for android

--- packages/mobile/react-native.config.js
+++ packages/mobile/react-native.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     project: {
         ios: {},
-        android: {},
+        android: {
+            sourceDir: "../../../app"
+        },
     },
     assets: ['../components/src/assets/fonts']
 };
