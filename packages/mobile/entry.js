a0b4f7307797d472b3a1729fb2599aa608837838
Neel Bakshi <neel@headout.com>
2020-02-13 00:14:49 +0530

    removed lib folder

--- packages/mobile/entry.js
+++ packages/mobile/entry.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Help from '../components/src/screens/HelpPage/HelpCenter';
+import Help from '../../components/src/screens/HelpPage/HelpCenter';
 import {NativeModules} from 'react-native';
 
 if (process.env.NODE_ENV !== 'production') {
-    const whyDidYouRender = require('@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js');
-    whyDidYouRender(React);
+  const whyDidYouRender = require('@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js');
+  whyDidYouRender(React);
 }
 
 export default class HelpScreen extends React.Component {
+  openListicle = (link, title) => {
+    NativeModules.HelpCenterNativeBridge.openLink(
+      link,
+      title,
+      this.props.rootTag,
+    );
+  };
 
-    openListicle = (link, title) => {
-        NativeModules.HelpCenterNativeBridge.openLink(link,title,this.props.rootTag);
-    }
-
-    render() {
-        return (
-            <Help openListicle={this.openListicle}/>
-        );
-    }
+  render() {
+    return <Help openListicle={this.openListicle} />;
+  }
 }
