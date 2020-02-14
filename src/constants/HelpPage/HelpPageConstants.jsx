352725314b7684c721e1ce7558d706b37af5e11b
Neel Bakshi <neel@headout.com>
2020-02-14 14:36:35 +0530

    fixed indentation in constants file

--- src/constants/HelpPage/HelpPageConstants.jsx
+++ src/constants/HelpPage/HelpPageConstants.jsx
@@ -1,164 +1,164 @@
 export const HELP_PAGE_CATEGORIES = [
-  {
-    HEADING: 'Booking',
-    OPTIONS: [
-      {
-        NAME: 'Experience duration & start time',
-        SRC:
-          'https://headout.kb.help/1-booking-details/experience-duration-start-time/',
-      },
-      {
-        NAME: 'Inclusions & exclusions',
-        SRC: 'https://headout.kb.help/1-booking-details/inclusions-exclusions/',
-      },
-      {
-        NAME: 'Meeting point & pick-up directions',
-        SRC:
-          'https://headout.kb.help/1-booking-details/meeting-point-pick-up-directions/',
-      },
-      {
-        NAME: 'Meeting point information',
-        SRC:
-          'https://headout.kb.help/1-booking-details/meeting-point-information/',
-      },
-      {
-        NAME: 'Instant confirmation',
-        SRC: 'https://headout.kb.help/1-booking-details/instant-confirmation/',
-      },
-      {
-        NAME: 'Mobile & Print Tickets',
-        SRC: 'https://headout.kb.help/1-booking-details/mobile-print-tickets/',
-      },
-    ],
-  },
-  {
-    HEADING: 'Payment',
-    OPTIONS: [
-      {
-        NAME: 'Best Price Guarantee',
-        SRC: 'https://headout.kb.help/2-payment/best-price-guarantee/',
-      },
-      {
-        NAME: 'Conversion Fee',
-        SRC: 'https://headout.kb.help/2-payment/conversion-fee/',
-      },
-      {
-        NAME: 'Payment Currencies',
-        SRC: 'https://headout.kb.help/2-payment/payment-currencies/',
-      },
-      {
-        NAME: 'Payment Methods',
-        SRC: 'https://headout.kb.help/2-payment/payment-methods/',
-      },
-      {
-        NAME: 'Refund Information',
-        SRC: 'https://headout.kb.help/2-payment/refund-information/',
-      },
-      {
-        NAME: 'Verifying Card Ownership',
-        SRC: 'https://headout.kb.help/2-payment/verifying-card-ownership/',
-      },
-      {
-        NAME: 'Payment Receipts',
-        SRC: 'https://headout.kb.help/2-payment/payment-receipts/',
-      },
-      {
-        NAME: 'Payment Failure',
-        SRC: 'https://headout.kb.help/2-payment/payment-failure/',
-      },
-    ],
-  },
-  {
-    HEADING: 'Cancellation & Refunds',
-    OPTIONS: [
-      {
-        NAME: 'Cancel Reservation',
-        SRC:
-          'https://headout.kb.help/3-cancellation-refunds/cancel-reservation/',
-      },
-      {
-        NAME: 'Cancellation Deadline',
-        SRC:
-          'https://headout.kb.help/3-cancellation-refunds/cancellation-deadline/',
-      },
-      {
-        NAME: 'Refund Information',
-        SRC:
-          'https://headout.kb.help/3-cancellation-refunds/refund-information/',
-      },
-    ],
-  },
-  {
-    HEADING: 'Booking Modification',
-    OPTIONS: [
-      {
-        NAME: 'Add/Remove Participant',
-        SRC:
-          'https://headout.kb.help/4-booking-modification/addremove-participant/',
-      },
-      {
-        NAME: 'Changing tour options',
-        SRC:
-          'https://headout.kb.help/4-booking-modification/changing-tour-options/',
-      },
-      {
-        NAME: 'Rescheduling a reservation',
-        SRC:
-          'https://headout.kb.help/4-booking-modification/rescheduling-a-reservation/',
-      },
-      {
-        NAME: 'Update Participant Information',
-        SRC:
-          'https://headout.kb.help/4-booking-modification/update-participant-information/',
-      },
-    ],
-  },
-  {
-    HEADING: 'Coupons & Cashback',
-    OPTIONS: [
-      {
-        NAME: 'Using Coupon Code',
-        SRC: 'https://headout.kb.help/5-coupons-cashback/using-coupon-code/',
-      },
-      {
-        NAME: 'Coupon Code Not Working',
-        SRC:
-          'https://headout.kb.help/5-coupons-cashback/coupon-code-not-working/',
-      },
-      {
-        NAME: 'Getting Cashback',
-        SRC: 'https://headout.kb.help/5-coupons-cashback/getting-cashback/',
-      },
-      {
-        NAME: 'Using Cashback',
-        SRC: 'https://headout.kb.help/5-coupons-cashback/using-cashback/',
-      },
-    ],
-  },
-  {
-    HEADING: 'General Queries',
-    OPTIONS: [
-      {
-        NAME: 'Group Bookings',
-        SRC:
-          'https://headout.kb.help/6-general-queries/customise-an-experience/',
-      },
-      {
-        NAME: 'Gift an experience',
-        SRC: 'https://headout.kb.help/6-general-queries/gift-an-experience/',
-      },
-      {
-        NAME: 'Headout in different languages',
-        SRC:
-          'https://headout.kb.help/6-general-queries/headout-in-different-languages/',
-      },
-    ],
-  },
+    {
+        HEADING: 'Booking',
+        OPTIONS: [
+            {
+                NAME: 'Experience duration & start time',
+                SRC:
+                    'https://headout.kb.help/1-booking-details/experience-duration-start-time/',
+            },
+            {
+                NAME: 'Inclusions & exclusions',
+                SRC: 'https://headout.kb.help/1-booking-details/inclusions-exclusions/',
+            },
+            {
+                NAME: 'Meeting point & pick-up directions',
+                SRC:
+                    'https://headout.kb.help/1-booking-details/meeting-point-pick-up-directions/',
+            },
+            {
+                NAME: 'Meeting point information',
+                SRC:
+                    'https://headout.kb.help/1-booking-details/meeting-point-information/',
+            },
+            {
+                NAME: 'Instant confirmation',
+                SRC: 'https://headout.kb.help/1-booking-details/instant-confirmation/',
+            },
+            {
+                NAME: 'Mobile & Print Tickets',
+                SRC: 'https://headout.kb.help/1-booking-details/mobile-print-tickets/',
+            },
+        ],
+    },
+    {
+        HEADING: 'Payment',
+        OPTIONS: [
+            {
+                NAME: 'Best Price Guarantee',
+                SRC: 'https://headout.kb.help/2-payment/best-price-guarantee/',
+            },
+            {
+                NAME: 'Conversion Fee',
+                SRC: 'https://headout.kb.help/2-payment/conversion-fee/',
+            },
+            {
+                NAME: 'Payment Currencies',
+                SRC: 'https://headout.kb.help/2-payment/payment-currencies/',
+            },
+            {
+                NAME: 'Payment Methods',
+                SRC: 'https://headout.kb.help/2-payment/payment-methods/',
+            },
+            {
+                NAME: 'Refund Information',
+                SRC: 'https://headout.kb.help/2-payment/refund-information/',
+            },
+            {
+                NAME: 'Verifying Card Ownership',
+                SRC: 'https://headout.kb.help/2-payment/verifying-card-ownership/',
+            },
+            {
+                NAME: 'Payment Receipts',
+                SRC: 'https://headout.kb.help/2-payment/payment-receipts/',
+            },
+            {
+                NAME: 'Payment Failure',
+                SRC: 'https://headout.kb.help/2-payment/payment-failure/',
+            },
+        ],
+    },
+    {
+        HEADING: 'Cancellation & Refunds',
+        OPTIONS: [
+            {
+                NAME: 'Cancel Reservation',
+                SRC:
+                    'https://headout.kb.help/3-cancellation-refunds/cancel-reservation/',
+            },
+            {
+                NAME: 'Cancellation Deadline',
+                SRC:
+                    'https://headout.kb.help/3-cancellation-refunds/cancellation-deadline/',
+            },
+            {
+                NAME: 'Refund Information',
+                SRC:
+                    'https://headout.kb.help/3-cancellation-refunds/refund-information/',
+            },
+        ],
+    },
+    {
+        HEADING: 'Booking Modification',
+        OPTIONS: [
+            {
+                NAME: 'Add/Remove Participant',
+                SRC:
+                    'https://headout.kb.help/4-booking-modification/addremove-participant/',
+            },
+            {
+                NAME: 'Changing tour options',
+                SRC:
+                    'https://headout.kb.help/4-booking-modification/changing-tour-options/',
+            },
+            {
+                NAME: 'Rescheduling a reservation',
+                SRC:
+                    'https://headout.kb.help/4-booking-modification/rescheduling-a-reservation/',
+            },
+            {
+                NAME: 'Update Participant Information',
+                SRC:
+                    'https://headout.kb.help/4-booking-modification/update-participant-information/',
+            },
+        ],
+    },
+    {
+        HEADING: 'Coupons & Cashback',
+        OPTIONS: [
+            {
+                NAME: 'Using Coupon Code',
+                SRC: 'https://headout.kb.help/5-coupons-cashback/using-coupon-code/',
+            },
+            {
+                NAME: 'Coupon Code Not Working',
+                SRC:
+                    'https://headout.kb.help/5-coupons-cashback/coupon-code-not-working/',
+            },
+            {
+                NAME: 'Getting Cashback',
+                SRC: 'https://headout.kb.help/5-coupons-cashback/getting-cashback/',
+            },
+            {
+                NAME: 'Using Cashback',
+                SRC: 'https://headout.kb.help/5-coupons-cashback/using-cashback/',
+            },
+        ],
+    },
+    {
+        HEADING: 'General Queries',
+        OPTIONS: [
+            {
+                NAME: 'Group Bookings',
+                SRC:
+                    'https://headout.kb.help/6-general-queries/customise-an-experience/',
+            },
+            {
+                NAME: 'Gift an experience',
+                SRC: 'https://headout.kb.help/6-general-queries/gift-an-experience/',
+            },
+            {
+                NAME: 'Headout in different languages',
+                SRC:
+                    'https://headout.kb.help/6-general-queries/headout-in-different-languages/',
+            },
+        ],
+    },
 ];
 
 export const BOOKING_FLOW_HELP_OPTIONS = {
-  CHECK_STATUS: 'CHECK_STATUS',
-  RESEND: 'RESEND_TICKETS',
-  CANCEL_BOOKING: 'CANCEL_BOOKING',
-  MODIFY_BOOKING: 'MODIFY_BOOKING',
+    CHECK_STATUS: 'CHECK_STATUS',
+    RESEND: 'RESEND_TICKETS',
+    CANCEL_BOOKING: 'CANCEL_BOOKING',
+    MODIFY_BOOKING: 'MODIFY_BOOKING',
 };
