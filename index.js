8bb7326339edb3fd7f253188b02b5a747ced06cd
Neel Bakshi <neel@headout.com>
2020-03-06 14:39:00 +0530

    integrated sentry

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { AppRegistry } from 'react-native';
 import CodePush from 'react-native-code-push';
 import HelpScreenStack from './src/screens/helpPage/helpCenterNavigation';
+import * as Sentry from '@sentry/react-native';
+
 
 console.disableYellowBox = true;
 
@@ -9,4 +11,8 @@ AppRegistry.registerComponent('HelpScreen', () => HelpScreenStack);
 CodePush.sync({
     checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
     installMode: CodePush.InstallMode.ON_NEXT_RESTART
+});
+
+Sentry.init({
+    dsn: 'https://43e6055371754efd961cec48b0b0cfe8@sentry.io/3806304',
 });
